ab57ddc653e53d9a56ac7a4bc9d71a8effe574d7
Gilmar-PY <roryoviedo111@gmail.com>
2024-05-30 22:30:11 -0500

    Actualizar script

--- Actividad8/ejercicio3/procesa_paralelo_dask.py
+++ Actividad8/ejercicio3/procesa_paralelo_dask.py
@@ -1,3 +1,14 @@
+
+#Ejercicio 3: Procesamiento paralelo de archivos con Dask
+#Descripción: Utiliza Dask para procesar un conjunto de archivos CSV en paralelo, realizando una agregación (por ejemplo, promedio de una columna específica).
+#Tareas:
+#    Leer varios archivos CSV con Dask.
+#    Procesar los archivos en paralelo para calcular el promedio de una columna específica.
+#    Combinar los resultados y obtener el promedio total.
+#Pistas:
+#   Usa dask.dataframe.read_csv para leer los archivos.
+#    Usa dask.dataframe para realizar las operaciones en paralelo.
+
 import dask.dataframe as dd
 
 def parallel_csv_processing(file_paths):
