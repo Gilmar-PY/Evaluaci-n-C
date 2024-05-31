93dd4d5d9797e190b8c10648fe0b214c40c46157
Gilmar <117695862+Gilmar-PY@users.noreply.github.com>
2024-05-31 14:20:05 -0500

    Update Evaluacion_de_modelosml.py

--- Actividad8/ejercicio8/Evaluacion_de_modelosml.py
+++ Actividad8/ejercicio8/Evaluacion_de_modelosml.py
@@ -17,13 +17,15 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from joblib import Parallel, delayed
 
+# Crea y entrena un modelo RandomForest con n_estimators árboles
 def evaluate_model(n_estimators, X_train, X_test, y_train, y_test):
     model = RandomForestClassifier(n_estimators=n_estimators)
     model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+    y_pred = model.predict(X_test)##predice las etiquetas
     return (n_estimators, accuracy_score(y_test, y_pred))
 
 def parallel_model_evaluation():
+ #carga los datos iris
     iris = load_iris()
     X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=42)
     n_estimators_list = [10, 50, 100, 200]
