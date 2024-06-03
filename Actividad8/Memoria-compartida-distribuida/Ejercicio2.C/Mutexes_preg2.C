c64baa3946d82bb7fb4b05178353ac494eaa56a1
Gilmar <117695862+Gilmar-PY@users.noreply.github.com>
2024-06-03 15:20:15 -0500

    Update Mutexes_preg2.C

--- Actividad8/Memoria-compartida-distribuida/Ejercicio2.C/Mutexes_preg2.C
+++ Actividad8/Memoria-compartida-distribuida/Ejercicio2.C/Mutexes_preg2.C
@@ -1,5 +1,5 @@
 
-//*Implementa un programa en C utilizando POSIX threads (pthread) que demuestre el 
+//**Implementa un programa en C utilizando POSIX threads (pthread) que demuestre el 
 uso de mutexes para proteger una variable compartida. El programa debe crear 
 varios hilos que incrementen una variable global compartida de manera segura.*//
 
