c5009ddf6a9d53bffe06f28ddd79c0438892e1e5
oviedo <oviedo@oviedo>
2024-06-11 00:53:10 -0500

    Eliminar todos los archivos

--- preg2.py
+++
@@ -1,8 +0,0 @@
-import threading
-
-def thread_function(name):
-    print(f"Thread {name} is running.")
-
-thread = threading.Thread(target=thread_function, args=(1,))
-thread.start()
-thread.join()
