9c185f43a279da00ef96d1cb6c7a4313ad6ca279
Gilmar <117695862+Gilmar-PY@users.noreply.github.com>
2024-06-27 22:32:27 -0500

    Update test_resiliencia.py

--- Proyecto_Final/sprint3/src/test_resiliencia.py
+++ Proyecto_Final/sprint3/src/test_resiliencia.py
@@ -1,1 +1,26 @@
 
+import requests
+
+def simular_fallo_nodo(nodo):
+    try:
+        response = requests.post(f"{nodo}/fallo")
+        if response.status_code == 200:
+            print(f"Fallo simulado exitosamente en {nodo}")
+        else:
+            print(f"Error simulando fallo en {nodo}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+def verificar_recuperacion_datos(nodo, archivo):
+    try:
+        response = requests.get(f"{nodo}/descargar/{archivo}")
+        if response.status_code == 200:
+            print(f"Recuperación de datos exitosa desde {nodo}")
+        else:
+            print(f"Error recuperando datos desde {nodo}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    simular_fallo_nodo("http://storage-node-2:5000")
+    verificar_recuperacion_datos("http://storage-node-1:5000", "Actividad.txt")
