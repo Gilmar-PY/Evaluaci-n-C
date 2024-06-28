405bbe3f7c6877c4a4d1a8305f9c298ea9307d12
Gilmar <117695862+Gilmar-PY@users.noreply.github.com>
2024-06-27 22:31:51 -0500

    Update test_seguridad.py

--- Proyecto_Final/sprint3/src/test_seguridad.py
+++ Proyecto_Final/sprint3/src/test_seguridad.py
@@ -1,1 +1,12 @@
 
+from app.routes import cifrar_archivo, descifrar_archivo
+
+def probar_cifrado_descifrado():
+    data = b"Datos de prueba"
+    nonce, tag, ciphertext = cifrar_archivo(data)
+    descifrado = descifrar_archivo(nonce, tag, ciphertext)
+    assert data == descifrado, "La prueba de cifrado y descifrado falló"
+
+if __name__ == "__main__":
+    probar_cifrado_descifrado()
+    print("Prueba de cifrado y descifrado exitosa")
