4043570027ac855a2ee2f32007ce3e024709069a
Gilmar <117695862+Gilmar-PY@users.noreply.github.com>
2024-06-28 14:20:27 -0500

    Update paxos.py

--- Actividad15/pregunta1/paxos.py
+++ Actividad15/pregunta1/paxos.py
@@ -1,1 +1,23 @@
 
+class Paxos:
+    def __init__(self):
+        self.proposal_number = 0
+        self.accepted_proposal = None
+
+    def propose(self, value):
+        self.proposal_number += 1
+        proposal = (self.proposal_number, value)
+        if self.accept(proposal):
+            self.accepted_proposal = proposal
+            return proposal
+        return None
+
+    def accept(self, proposal):
+        if self.accepted_proposal is None or proposal[0] > self.accepted_proposal[0]:
+            return True
+        return False
+
+# Ejemplo de uso
+paxos = Paxos()
+print(paxos.propose('update1'))
+print(paxos.propose('update2'))
