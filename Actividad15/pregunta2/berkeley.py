0e775d09df2a27a0206830f6acbeeb32335e22a7
Gilmar <117695862+Gilmar-PY@users.noreply.github.com>
2024-06-28 14:53:44 -0500

    Update berkeley.py

--- Actividad15/pregunta2/berkeley.py
+++ Actividad15/pregunta2/berkeley.py
@@ -1,26 +1,30 @@
-
 class BerkeleyNode:
     def __init__(self, node_id, time):
-        self.node_id = node_id
-        self.time = time
+        self.node_id = node_id  # ID único del nodo
+        self.time = time  # Tiempo actual del nodo
 
     def adjust_time(self, offset):
-        self.time += offset
+        self.time += offset  # Ajusta el tiempo del nodo sumando el offset
 
 class BerkeleyMaster:
     def __init__(self, nodes):
-        self.nodes = nodes
+        self.nodes = nodes  # Lista de nodos a sincronizar
 
     def synchronize_clocks(self):
-        times = [node.time for node in self.nodes]
-        average_time = sum(times) / len(times)
+        times = [node.time for node in self.nodes]  # Obtiene los tiempos actuales de todos los nodos
+        average_time = sum(times) / len(times)  # Calcula el tiempo promedio de todos los nodos
         for node in self.nodes:
-            offset = average_time - node.time
-            node.adjust_time(offset)
-        return [(node.node_id, node.time) for node in self.nodes]
+            offset = average_time - node.time  # Calcula el offset para cada nodo
+            node.adjust_time(offset)  # Ajusta el tiempo de cada nodo según el offset calculado
+        return [(node.node_id, node.time) for node in self.nodes]  # Retorna una lista de tuplas (ID del nodo, tiempo sincronizado)
 
 # Ejemplo de uso
-nodes = [BerkeleyNode(i, time) for i, time in enumerate([10, 20, 30])]
-master = BerkeleyMaster(nodes)
-synchronized_times = master.synchronize_clocks()
-print(synchronized_times)
+nodes = [BerkeleyNode(i, time) for i, time in enumerate([10, 20, 30])]  # Crea una lista de nodos con tiempos iniciales
+master = BerkeleyMaster(nodes)  # Crea una instancia del maestro de Berkeley con los nodos
+synchronized_times = master.synchronize_clocks()  # Sincroniza los relojes de los nodos
+print(synchronized_times)  # Imprime los tiempos sincronizados de cada nodo
+
+'''
+Este algoritmo de Berkeley busca ajustar los relojes de los nodos para que se acerquen
+al tiempo promedio de todos los nodos, minimizando las
+discrepancias entre ellos y mejorando la sincronización en un sistema distribuido. '''
