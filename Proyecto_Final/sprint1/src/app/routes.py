f2557e8eb22c4e334e82bd8c375dc69cbd449a26
Gilmar <117695862+Gilmar-PY@users.noreply.github.com>
2024-06-29 23:33:10 -0500

    Update routes.py

--- Proyecto_Final/sprint1/src/app/routes.py
+++ Proyecto_Final/sprint1/src/app/routes.py
@@ -4,12 +4,11 @@ import os
 from Crypto.Cipher import AES
 import logging
 
-UPLOAD_FOLDER = 'cargas'
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), '..', 'cargas')
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # Clave de cifrado de 16 bytes
 key = b'This_is_a16b_key'
-assert len(key) == 16, f"Longitud de la clave AES incorrecta: {len(key)}"
 
 logging.basicConfig(level=logging.DEBUG)
 logging.debug(f"Longitud de la clave AES: {len(key)}")  # Esto debe imprimir 16
@@ -32,6 +31,7 @@ def cargar_archivo():
             file_enc.write(ciphertext)
         
         logging.info(f"Archivo {archivo.filename} cargado y cifrado exitosamente")
+        logging.debug(f"Ruta del archivo cifrado: {ruta_archivo}")
         return 'Archivo cargado y cifrado exitosamente', 200
     except Exception as e:
         logging.error(f"Error al cargar y cifrar el archivo: {e}")
@@ -41,8 +41,6 @@ def cargar_archivo():
 def descargar_archivo(nombre_archivo):
     try:
         ruta_archivo = os.path.join(UPLOAD_FOLDER, nombre_archivo)
-        logging.debug(f"Ruta del archivo cifrado: {ruta_archivo}")
-
         with open(ruta_archivo, 'rb') as file_enc:
             nonce = file_enc.read(16)
             tag = file_enc.read(16)
@@ -52,26 +50,15 @@ def descargar_archivo(nombre_archivo):
         data = cipher.decrypt_and_verify(ciphertext, tag)
 
         temp_file_path = os.path.join(UPLOAD_FOLDER, f"temp_{nombre_archivo}")
-        logging.debug(f"Ruta del archivo temporal: {temp_file_path}")
-
         with open(temp_file_path, 'wb') as temp_file:
             temp_file.write(data)
 
+        logging.debug(f"Ruta del archivo temporal: {temp_file_path}")
         logging.debug(f"Archivo temporal creado: {temp_file_path}")
-
-        # Verifica que el archivo temporal realmente existe
-        if not os.path.exists(temp_file_path):
-            logging.error(f"El archivo temporal no existe: {temp_file_path}")
-            raise FileNotFoundError(f"No such file or directory: '{temp_file_path}'")
-
-        # Envía el archivo desde la ruta correcta
-        return send_file(temp_file_path, as_attachment=True)
+        return send_file(temp_file_path, as_attachment=True, download_name=nombre_archivo)
     except Exception as e:
         logging.error(f"Error al descargar y descifrar el archivo: {e}")
         return jsonify({'error': f'Error al descargar y descifrar el archivo: {e}'}), 500
-
-
-
 
 @app.route('/eliminar/<nombre_archivo>', methods=['DELETE'])
 def eliminar_archivo(nombre_archivo):
