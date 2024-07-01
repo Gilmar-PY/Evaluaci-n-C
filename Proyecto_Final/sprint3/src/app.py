8f188f0faf7a48b38e8918f90b37d161d95d5c72
Gilmar <117695862+Gilmar-PY@users.noreply.github.com>
2024-07-01 04:00:58 -0500

    Update app.py

--- Proyecto_Final/sprint3/src/app.py
+++ Proyecto_Final/sprint3/src/app.py
@@ -1,5 +1,11 @@
-
 from app import app
+from app.routes import num_worker_threads, queue, threads
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)   
+if __name__ == "__main__":
+    try:
+        app.run(host="0.0.0.0", port=5000, debug=True)
+    finally:
+        for _ in range(num_worker_threads):
+            queue.put((None, None, None, None))
+        for t in threads:
+            t.join()
