2420ba91cc65c45f24c55a974336c099d4e8ab9b
Gilmar <117695862+Gilmar-PY@users.noreply.github.com>
2024-07-01 04:05:53 -0500

    Update __init__.py

--- Proyecto_Final/sprint3/src/app/__init__.py
+++ Proyecto_Final/sprint3/src/app/__init__.py
@@ -1,6 +1,15 @@
-
 from flask import Flask
+import os
+import logging
 
 app = Flask(__name__)
+UPLOAD_FOLDER = '/app/cargas'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-from app import routes
+logging.basicConfig(level=logging.DEBUG)
+
+from app.routes import cargar_archivo, descargar_archivo, eliminar_archivo
+
+app.add_url_rule('/cargar', 'cargar_archivo', cargar_archivo, methods=['POST'])
+app.add_url_rule('/descargar/<nombre_archivo>', 'descargar_archivo', descargar_archivo, methods=['GET'])
+app.add_url_rule('/eliminar/<nombre_archivo>', 'eliminar_archivo', eliminar_archivo, methods=['DELETE'])
