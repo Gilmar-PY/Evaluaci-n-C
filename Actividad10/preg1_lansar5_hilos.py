4007684fe6cf8434bd95871df9343c7c0fd2bce8
Gilmar <117695862+Gilmar-PY@users.noreply.github.com>
2024-07-10 01:29:26 -0500

    Update preg1_lansar5_hilos.py

--- Actividad10/preg1_lansar5_hilos.py
+++ Actividad10/preg1_lansar5_hilos.py
@@ -1,1 +1,15 @@
+import threading
+
+def print_id(thread_id):
+    for _ in range(10):
+        print(f"Hilo {thread_id}")
+
+threads = []
+for i in range(5):
+    thread = threading.Thread(target=print_id, args=(i,))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
 
